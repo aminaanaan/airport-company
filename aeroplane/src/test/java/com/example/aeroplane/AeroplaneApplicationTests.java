24900a90ff77fc44ee9242acedc167d6509512ae
aminaanaan <amina-mo1@hotmail.com>
2020-05-16 20:14:04 +0200

    add spring boot project for entities

---
+++ aeroplane/src/test/java/com/example/aeroplane/AeroplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aeroplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroplaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
