38f8b581feebb8fee32a43d5435ad9ee84161c7a
aminaanaan <amina-mo1@hotmail.com>
2020-05-18 14:55:43 +0200

    add api for user

--- aeroplane/src/main/java/com/example/aeroplane/rest/RadnikController.java
+++ aeroplane/src/main/java/com/example/aeroplane/rest/RadnikController.java
@@ -1,6 +1,8 @@
 package com.example.aeroplane.rest;
 
+import com.example.aeroplane.dao.*;
 import com.example.aeroplane.dto.*;
+import com.example.aeroplane.repository.*;
 import com.example.aeroplane.validation.*;
 import io.swagger.annotations.*;
 import lombok.*;
@@ -8,17 +10,28 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.*;
+
 @AllArgsConstructor
 @RestController
-@RequestMapping("/incident")
+@RequestMapping("/radnik")
 public class RadnikController {
 
     @Autowired
     RequestValidation requestValidation;
+    @Autowired
+    KorisnikRepository radnikRepository;
     @ApiOperation(value = "Login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping("/login")
     String login(@RequestBody KorisnikLoginRequest korisnikLoginRequest) throws Exception {
         requestValidation.validateLogin(korisnikLoginRequest);
         return "Successfully logged in!";
+    }
+
+    @ApiOperation(value = "Create User", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping()
+    public String create(@Valid @RequestBody Radnik korisnik) {
+         radnikRepository.save(korisnik);
+         return "Radnik sacuvan";
     }
 }
